04d830a5d137cc666245991ce87b4b2b348f77a1
hp <hp@Ritesh>
2017-09-01 10:09:31 +0530

    First

--- NewJavaLab/src/Jva.java
+++ NewJavaLab/src/Jva.java
@@ -1,14 +1,15 @@
 
 public class Jva {
-public static void main(String args[]) {
-	System.out.println("First");
-	System.out.println("Second");
-//<<<<<<< HEAD
-	System.out.println("Master changes");
-//=======
-	System.out.println("child 1 works");
-	System.out.println("child 1 works changes");
-
-//>>>>>>> refs/heads/Child1
-}
+	public static void main(String args[]) {
+		// int arr[4];
+		int arr[] = new int[5];// NO ERROR
+		// int b[]=new int[5] {1,2,3,4,5};//ERROR
+		int c[] = new int[] { 1, 2, 3, 4, 5 };// no error
+		int d[][] = new int[5][];// no error
+		// int e[][]=new int[][5];//Error
+	int k[][]=new int[4][3];
+	k[0]=new int[4];
+	k[1]=new int[2];
+	k=new int[3][2]; 
+	}
 }
